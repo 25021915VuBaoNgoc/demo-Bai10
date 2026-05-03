01ae9f1aee24fa5c04b7947aed7052a93144f2c8
25021915VuBaoNgoc <25021915@vnu.edu.vn>
2026-05-03 21:08:39 +0700

    sửa lỗi tự thêm

--- src/main/java/com/lab/ShippingCalculator.java
+++ src/main/java/com/lab/ShippingCalculator.java
@@ -6,6 +6,9 @@ public class ShippingCalculator {
     if (weight <= 0) {
       throw new IllegalArgumentException("Weight must be positive");
     }
+    if (type == null){
+      throw new IllegalArgumentException("type must not be null");
+    }
     if (type.equals("EXPRESS"))
       return weight * 5000 + 20000;
     if (type.equals("STANDARD"))
